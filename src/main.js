08b2c07cdaa2b493004cc216c9c27a0bb4ec0361
OnishYurii <onishchenkoyurii@gmail.com>
2023-11-24 14:02:45 +0200

    Updated

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
-import { QueryAPI } from './JS/first-fetch';
+import { getProductsByParams } from './JS/API';
 import { appendMarkup } from './JS/markup-product-cards';
 
-const queryAPI = new QueryAPI();
 window.addEventListener('resize', fetchBasedOnScreenSize);
 fetchBasedOnScreenSize();
+
 function fetchBasedOnScreenSize() {
+  const options = {
+    keyword: '',
+    page: 1,
+    limit: '',
+  };
   const windowWidth = window.innerWidth;
   if (windowWidth < 768) {
-    queryAPI.limit = 6;
-    queryAPI
-      .searchingQuery()
+    options.limit = 6;
+    getProductsByParams(options)
       .then(data => appendMarkup(data))
       .catch(er => console.log(er));
   } else if (windowWidth >= 768 && windowWidth < 1440) {
-    queryAPI.limit = 8;
-    queryAPI
-      .searchingQuery()
+    options.limit = 8;
+    getProductsByParams(options)
       .then(data => appendMarkup(data))
       .catch(er => console.log(er));
   } else {
-    queryAPI.limit = 9;
-    queryAPI
-      .searchingQuery()
+    options.limit = 9;
+    getProductsByParams(options)
       .then(data => appendMarkup(data))
       .catch(er => console.log(er));
   }
