d1ade5e1b102896efdb849178f8b1cd97f3c7b27
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-11-25 17:58:14 +0200

    upd footer-subscription

--- src/JS/footer-subscription.js
+++ src/JS/footer-subscription.js
@@ -1,15 +1,52 @@
-import { orderSubscription } from "./API";
+import { orderSubscription } from './API';
 
-const form = document.querySelector('.footer-form');
-
-form.addEventListener('submit', onSubscribe);
-
-const email = 'dasha564@gmail.com';
-
-async function onSubscribe() {
+try {
+    const form = document.querySelector('.footer-form');
+    form.addEventListener('submit', onSubscribe);
+  async function onSubscribe() {
     // e.preventDefault();
+    const email = form.querySelector('#footer-email').value;
+
+    if (!email) {
+      throw new Error('Email is required!');
+    }
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(email)) {
+      throw new Error('Invalid email format!');
+    }
+
     const message = await orderSubscription(email);
     console.log(message);
+  }
+
+  onSubscribe();
+} catch (error) {
+  console.error(error);
 }
 
-onSubscribe();
+// -------II variant------
+// import { orderSubscription } from "./API";
+
+// const form = document.querySelector('.footer-form');
+// const emailInput = document.querySelector('.form-input');
+// const submitButton = document.querySelector('.footer-form-btn');
+
+// form.addEventListener('submit', onSubscribe);
+
+// async function onSubscribe(e) {
+//     e.preventDefault();
+//     const email = emailInput.value;
+
+//     if (validateEmail(email)) {
+//          const message = await orderSubscription(email);
+//         console.log(message);
+//     } else {
+//         console.log('Invalid email');
+//     }
+// }
+
+// function validateEmail(email) {
+//   const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   return pattern.test(email);
+// }
