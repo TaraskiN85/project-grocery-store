ae9b17565ad1dd419b3f21cf553b26d793a421b7
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-11-27 19:29:34 +0200

    update footer and cart

--- src/JS/modal-cart.js
+++ src/JS/modal-cart.js
@@ -1,9 +1,20 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const cartBtnSubmit = document.querySelector('.cart_form_button')
 const modalCartByBuy = document.querySelector('.modal-cart')
 const modalCartClose = document.querySelector('.close-btn')
 
 
-function displayByBuyModal() {
+function displayByBuyModal(event) {
+  event.preventDefault();
+
+  const emailInputValue = document.querySelector('.cart_form_input').value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailInputValue)) {
+    alert('Please, enter valid Email.');
+  //   Notify.failure('Please, enter valid Email.');
+    return;
+  }
     modalCartByBuy.classList.remove('is-hidden')
     document.body.style.overflow = 'hidden'; 
   }
