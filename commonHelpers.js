91beb75b7cb1f8cf8e25381a225e187a8da0e9f5
lMarinl <136239542+lMarinl@users.noreply.github.com>
2023-11-28 12:39:37 +0000

    Deploying to gh-pages from @ TaraskiN85/project-grocery-store@a1dd92757dffa9123c0ac708a32b1a4da62d0ed3 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import"./assets/footer-subscription-05dc7ea4.js";import"./assets/vendor-8a888f22.js";const r=document.querySelector(".cart-list"),p=JSON.parse(localStorage.getItem("cart-products-list")),m=t=>t.map(({_id:e,name:c,img:o,category:i,price:l,size:u,popularity:L,quantity:h,is10PercentOff:$,desc:k})=>`<li class="card_container_product" id="${e}">
+import"./assets/footer-subscription-05dc7ea4.js";import"./assets/vendor-8a888f22.js";const o=document.querySelector(".cart-list"),p=JSON.parse(localStorage.getItem("cart-products-list")),s=e=>e.map(({_id:t,name:c,img:r,category:i,price:l,size:u,popularity:I,quantity:h,is10PercentOff:$,desc:k})=>`<li class="card_container_product" id="${t}">
 
          <div class="description">
           <div class="card_container-product_img">
-            <img class="card_product_img" src="${o}" alt="${c}" />
+            <img class="card_product_img" src="${r}" alt="${c}" />
           </div>
 
           <div class="card_container_product_fullDs">
@@ -24,13 +24,13 @@ import"./assets/footer-subscription-05dc7ea4.js";import"./assets/vendor-8a888f22
             </div>
           </div>
           <div class="card-product-delete-button">
-            <button type="button" class="cart-product-delete-button">
+            <button type="button" class="cart-product-delete-button id="${t}">
               <svg class="cart-icon-close-" width="18" height="18">
-                <use src="../img/icons.svg#icon-delete-close"></use>
+                <use src="./img/icons.svg#icon-delete-close"></use>
               </svg>
             </button>
           </div>
         </div>
 
-    </li>`).join(""),d=()=>{r.innerHTML="",r.insertAdjacentHTML("afterbegin",m(p))};d();const s=document.querySelector(".card-product-delete-button").parentNode,_=document.querySelector(".cart-product-delete-all-button").parentNode;s.addEventListener("click",v);_.addEventListener("click",g);console.log(s);function v(){const t=s.parentNode.id;console.log(t);const e=JSON.parse(localStorage.getItem("cart-products-list"));console.log(e);const c=e.filter(o=>o._id!==t);localStorage.setItem("cart-products-list",JSON.stringify(c)),console.log(c),r.innerHTML="",d()}function g(){localStorage.setItem("cart-products-list",JSON.stringify([])),r.innerHTML=""}const y=document.querySelector(".cart_form_button"),n=document.querySelector(".modal-cart"),f=document.querySelector(".close-btn");function S(t){t.preventDefault();const e=document.querySelector(".cart_form_input").value;if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)){alert("Please, enter valid Email.");return}n.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function b(t){t.target===n&&a()}function a(){n.classList.add("is-hidden"),document.body.style.overflow="visible"}y.addEventListener("click",S);window.addEventListener("click",b);f.addEventListener("click",a);
+    </li>`).join(""),m=()=>{o.innerHTML="",o.insertAdjacentHTML("afterbegin",s(p))},_=()=>{const e=JSON.parse(localStorage.getItem("cart-products-list"))||[];o.innerHTML="",o.insertAdjacentHTML("afterbegin",s(e))};m();function n(){const t=(JSON.parse(localStorage.getItem("cart-products-list"))||[]).length,c=document.querySelector(".quantity_products"),r=document.querySelector(".cart-span");c.textContent="("+t+")",r.textContent="("+t+")"}n();const g=document.querySelector(".cart-product-delete-all-button");function v(e){let t=JSON.parse(localStorage.getItem("cart-products-list"))||[];const c=t.findIndex(r=>r._id===e);c!==-1?(t.splice(c,1),localStorage.setItem("cart-products-list",JSON.stringify(t)),_(),n()):console.log("Product not found in cart")}const y=document.querySelector(".cart-list");async function f(e){const t=e.target.closest(".card-product-delete-button");if(t){const c=t.closest(".card_container_product").id;v(c)}}y.addEventListener("click",f);function S(){localStorage.setItem("cart-products-list",JSON.stringify([])),o.innerHTML="",n()}g.addEventListener("click",S);const C=document.querySelector(".cart_form_button"),a=document.querySelector(".modal-cart"),b=document.querySelector(".close-btn");function L(e){e.preventDefault();const t=document.querySelector(".cart_form_input").value;if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)){alert("Please, enter valid Email.");return}a.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function q(e){e.target===a&&d()}function d(){a.classList.add("is-hidden"),document.body.style.overflow="visible"}C.addEventListener("click",L);window.addEventListener("click",q);b.addEventListener("click",d);
 //# sourceMappingURL=commonHelpers.js.map
