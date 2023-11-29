efb57d8dfc7eb6cd59501a8ae5a0376031313134
TaraskiN85 <73799167+TaraskiN85@users.noreply.github.com>
2023-11-29 16:18:48 +0000

    Deploying to gh-pages from @ TaraskiN85/project-grocery-store@0e051dcce33a6d9217ffa1bfff4e896f0ae25c1a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{s as i,c as g}from"./assets/footer-subscription-0dc6c13c.js";import"./assets/vendor-8a888f22.js";const a=document.querySelector(".cart-list"),_=JSON.parse(localStorage.getItem("cart-products-list")),l=e=>e.map(({_id:t,name:c,img:r,category:o,price:p,size:m,popularity:E,quantity:M,is10PercentOff:N,desc:j})=>`<li class="card_container_product" id="${t}">
+import{s as l,c as _}from"./assets/footer-subscription-6a804ea6.js";import"./assets/vendor-8a888f22.js";const a=document.querySelector(".cart-list"),y=JSON.parse(localStorage.getItem("cart-products-list")),u=e=>e.map(({_id:t,name:c,img:r,category:s,price:o,size:g,popularity:k,quantity:j,is10PercentOff:E,desc:M})=>`<li class="card_container_product" id="${t}">
 
          <div class="description">
           <div class="card_container-product_img">
@@ -12,15 +12,15 @@ import{s as i,c as g}from"./assets/footer-subscription-0dc6c13c.js";import"./ass
             <div class="card_container_product_description">
               <div class="card-description">
                 <p class="cart_product_description">
-                  <span class="cart_product_span">Category:</span>${o}
+                  <span class="cart_product_span">Category:</span>${s}
                 </p>
                 <p class="cart_product_description">
-                  <span class="cart_product_span">Size:</span>${m}
+                  <span class="cart_product_span">Size:</span>${g}
                 </p>
               </div>
             </div>
             <div class="card_container_price">
-              <p class="card_product_price">$${p}</p>
+              <p class="card_product_price">$${o}</p>
             </div>
           </div>
           <div class="card-product-delete-button">
@@ -32,5 +32,5 @@ import{s as i,c as g}from"./assets/footer-subscription-0dc6c13c.js";import"./ass
           </div>
         </div>
 
-    </li>`).join(""),S=()=>{a.innerHTML="",a.insertAdjacentHTML("afterbegin",l(_))},y=()=>{const e=JSON.parse(localStorage.getItem("cart-products-list"))||[];a.innerHTML="",a.insertAdjacentHTML("afterbegin",l(e))},s=document.querySelector(".modal-cart"),v=document.querySelector(".close-btn");function f(){s.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function b(e){e.target===s&&u()}function u(){s.classList.add("is-hidden"),document.body.style.overflow="visible"}window.addEventListener("click",b);v.addEventListener("click",u);S();function n(){const t=(JSON.parse(localStorage.getItem("cart-products-list"))||[]).length,c=document.querySelector(".quantity_products"),r=document.querySelector(".cart-span");c.textContent="("+t+")",r.textContent="("+t+")"}n();const C=document.querySelector(".cart-product-delete-all-button");function L(e){let t=JSON.parse(localStorage.getItem("cart-products-list"))||[];const c=t.findIndex(r=>r._id===e);c!==-1?(t.splice(c,1),localStorage.setItem("cart-products-list",JSON.stringify(t)),y(),n()):console.log("Product not found in cart")}const P=document.querySelector(".cart-list");async function I(e){const t=e.target.closest(".card-product-delete-button");if(t){const c=t.closest(".card_container_product").id;L(c)}d()}P.addEventListener("click",I);function h(){localStorage.setItem("cart-products-list",JSON.stringify([])),a.innerHTML="",n(),d()}C.addEventListener("click",h);const q=document.querySelector(".cart_total_cost");function d(){const t=JSON.parse(localStorage.getItem("cart-products-list")).reduce((c,r)=>c+r.price,0);q.textContent=`$ ${t.toFixed(2)}`}d();const O=document.querySelector(".cart_form_button");O.addEventListener("click",k);function $(){localStorage.removeItem("cart-products-list")}async function k(){const e=document.querySelector(".cart_form_input").value,t=JSON.parse(localStorage.getItem("cart-products-list"));if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)){i("Please, enter valid Email!");return}const r={email:e,products:t};console.log(r);try{const o=await g(r);$(),f(o)}catch(o){console.error("Error creating order:",o),i("Error creating order. Please try again later.")}}
+    </li>`).join(""),S=()=>{a.innerHTML="",a.insertAdjacentHTML("afterbegin",u(y))},v=()=>{const e=JSON.parse(localStorage.getItem("cart-products-list"))||[];a.innerHTML="",a.insertAdjacentHTML("afterbegin",u(e))},n=document.querySelector(".modal-cart"),f=document.querySelector(".close-btn");function b(){n.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function C(e){e.target===n&&p()}function p(){n.classList.add("is-hidden"),document.body.style.overflow="visible"}window.addEventListener("click",C);f.addEventListener("click",p);S();function d(){const t=(JSON.parse(localStorage.getItem("cart-products-list"))||[]).length,c=document.querySelector(".quantity_products"),r=document.querySelector(".cart-span");c.textContent="("+t+")",r.textContent="("+t+")"}d();const P=document.querySelector(".cart-product-delete-all-button");function I(e){let t=JSON.parse(localStorage.getItem("cart-products-list"))||[];const c=t.findIndex(r=>r._id===e);c!==-1?(t.splice(c,1),localStorage.setItem("cart-products-list",JSON.stringify(t)),v(),d()):console.log("Product not found in cart")}const L=document.querySelector(".cart-list");async function q(e){const t=e.target.closest(".card-product-delete-button");if(t){const c=t.closest(".card_container_product").id;I(c)}i()}L.addEventListener("click",q);function m(){localStorage.setItem("cart-products-list",JSON.stringify([])),a.innerHTML="",d(),i()}P.addEventListener("click",m);const h=document.querySelector(".cart_total_cost");function i(){const t=JSON.parse(localStorage.getItem("cart-products-list")).reduce((c,r)=>c+r.price,0);h.textContent=`$ ${t.toFixed(2)}`}i();const O=document.querySelector(".cart_form_button");O.addEventListener("click",$);async function $(){const e=document.querySelector(".cart_form_input").value,c=JSON.parse(localStorage.getItem("cart-products-list")).map(o=>({productId:o._id,amount:o.amount}));if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)){l("Please, enter valid Email!");return}const s={email:e,products:c};try{const o=await _(s);m(),b(o)}catch(o){console.error("Error creating order:",o),l("Error creating order. Please try again later.")}}
 //# sourceMappingURL=commonHelpers.js.map
