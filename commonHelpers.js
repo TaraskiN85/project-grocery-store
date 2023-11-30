53d0406d51a847ac530137407a12ef9def748547
TaraskiN85 <73799167+TaraskiN85@users.noreply.github.com>
2023-11-30 15:24:58 +0000

    Deploying to gh-pages from @ TaraskiN85/project-grocery-store@6bf2dd53b2a110c6576e94f59cf4820e5172e1e6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";import"./assets/vendor-8a888f22.js";const o=document.querySelector(".cart");JSON.parse(localStorage.getItem("cart-products-list"));const S=e=>{const t=e.map(({_id:c,name:a,img:l,category:s,price:h,size:b,popularity:x,quantity:N,is10PercentOff:P,desc:w})=>`<li class="card_container_product" id="${c}">
+import{i as o,s as p,c as f}from"./assets/footer-subscription-115f49fa.js";import"./assets/vendor-8a888f22.js";const l=document.querySelector(".cart");JSON.parse(localStorage.getItem("cart-products-list"));const y=t=>{const e=t.map(({_id:c,name:a,img:r,category:s,price:b,size:h,popularity:x,quantity:$,is10PercentOff:w,desc:P})=>`<li class="card_container_product" id="${c}">
 
          <div class="description">
           <div class="card_container-product_img">
-            <img class="card_product_img" src="${l}" alt="${a}" />
+            <img class="card_product_img" src="${r}" alt="${a}" />
           </div>
 
           <div class="card_container_product_fullDs">
@@ -15,18 +15,18 @@ import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";impor
                   <span class="cart_product_span">Category:</span>${s}
                 </p>
                 <p class="cart_product_description">
-                  <span class="cart_product_span">Size:</span>${b}
+                  <span class="cart_product_span">Size:</span>${h}
                 </p>
               </div>
             </div>
             <div class="card_container_price">
-              <p class="card_product_price">$${h}</p>
+              <p class="card_product_price">$${b}</p>
             </div>
           </div>
           <div class="card-product-delete-button">
-            <button type="button" class="cart-product-delete-button id="${c}">
+            <button type="button" class="cart-product-delete-button" id="${c}">
               <svg class="cart-icon-close-" width="18" height="18">
-                 <use href="${r}#icon-delete-close"></use>
+                 <use href="${o}#icon-delete-close"></use>
               </svg>
             </button>
           </div>
@@ -36,7 +36,7 @@ import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";impor
   <ul class="cart_list">
     <li class="cart-item">
       <svg class="cart-svg" width="18" height="18">
-        <use href="${r}#icon-cart"></use>
+        <use href="${o}#icon-cart"></use>
       </svg>
     </li>
     <li class="cart-item">
@@ -49,12 +49,12 @@ import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";impor
         <span class="cart-product-delete-span">Delete all</span>
         <button type="button" class="cart-product-delete-all-button">
           <svg class="cart-icon-delete-all" width="24" height="24">
-            <use href="${r}#icon-delete-close"></use>
+            <use href="${o}#icon-delete-close"></use>
           </svg>
         </button>
       </div>
       <ul class="cart-list">
-        ${t}
+        ${e}
       </ul>
     </div>
     <div class="cart_container_total_price">
@@ -79,13 +79,13 @@ import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";impor
       </div>
     </div>
   </div>
-</div>`},n=()=>{const e=JSON.parse(localStorage.getItem("cart-products-list"))||[];if(e.length>0)console.log("full"),o.innerHTML="",o.insertAdjacentHTML("afterbegin",S(e));else{o.innerHTML="",console.log("empty");const t=document.querySelector(".cart");t.innerHTML+=`<div class="container">
+</div>`},n=()=>{const t=JSON.parse(localStorage.getItem("cart-products-list"))||[];if(t.length>0)console.log("full"),l.innerHTML="",l.insertAdjacentHTML("afterbegin",y(t));else{l.innerHTML="",console.log("empty");const e=document.querySelector(".cart");e.innerHTML+=`<div class="container">
     <div class="empty-basket">
       <div class="basket_container">
         <ul class="basket_list">
           <li class="basket_item">
             <svg class="basket_svg" width="18" height="18">
-              <use href="${r}#icon-cart"></use>
+              <use href="${o}#icon-cart"></use>
             </svg>
           </li>
           <li class="basket_item">
@@ -111,5 +111,5 @@ import{i as r,s as _,c as f}from"./assets/footer-subscription-115f49fa.js";impor
         </p>
       </div>
     </div>
-  </div>`}},i=document.querySelector(".modal-cart"),y=document.querySelector(".close-btn");function k(){i.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function C(e){e.target===i&&m()}function m(){i.classList.add("is-hidden"),document.body.style.overflow="visible"}window.addEventListener("click",C);y.addEventListener("click",m);n();function d(){const t=(JSON.parse(localStorage.getItem("cart-products-list"))||[]).length,c=document.querySelector(".quantity_products"),a=document.querySelector(".cart-span");c.textContent="("+t+")",JSON.parse(localStorage.getItem("cart-products-list")).length&&(a.textContent="("+t+")")}d();const I=document.querySelector(".cart-product-delete-all-button");function L(e){let t=JSON.parse(localStorage.getItem("cart-products-list"))||[];const c=t.findIndex(a=>a._id===e);c!==-1&&(t.splice(c,1),localStorage.setItem("cart-products-list",JSON.stringify(t)),n(),d())}const g=document.querySelector(".cart-list");function u(e){const t=e.target.closest(".card-product-delete-button");if(t){const c=t.closest(".card_container_product").id;L(c),p()}}document.addEventListener("click",u);JSON.parse(localStorage.getItem("cart-products-list")).length&&g.addEventListener("click",u);function v(){localStorage.setItem("cart-products-list",JSON.stringify([])),o.innerHTML="",d(),p(),g.removeEventListener("click",u),n()}JSON.parse(localStorage.getItem("cart-products-list")).length&&I.addEventListener("click",v);const O=document.querySelector(".cart_total_cost");function p(){const t=JSON.parse(localStorage.getItem("cart-products-list")).reduce((c,a)=>c+a.price,0);JSON.parse(localStorage.getItem("cart-products-list")).length&&(O.textContent=`$ ${t.toFixed(2)}`)}p();const $=document.querySelector(".cart_form_button");JSON.parse(localStorage.getItem("cart-products-list")).length&&$.addEventListener("click",q);async function q(){const e=document.querySelector(".cart_form_input").value,c=JSON.parse(localStorage.getItem("cart-products-list")).map(s=>({productId:s._id,amount:s.amount}));if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)){_("Please, enter valid Email!");return}const l={email:e,products:c};try{const s=await f(l);v(),k(s)}catch(s){console.error("Error creating order:",s),_("Error creating order. Please try again later.")}}
+  </div>`}},i=document.querySelector(".modal-cart"),S=document.querySelector(".close-btn");function k(){i.classList.remove("is-hidden"),document.body.style.overflow="hidden"}function C(t){t.target===i&&_()}function _(){i.classList.add("is-hidden"),document.body.style.overflow="visible"}window.addEventListener("click",C);S.addEventListener("click",_);n();function d(){const e=(JSON.parse(localStorage.getItem("cart-products-list"))||[]).length,c=document.querySelector(".quantity_products"),a=document.querySelector(".cart-span");c.textContent="("+e+")",JSON.parse(localStorage.getItem("cart-products-list")).length&&(a.textContent="("+e+")")}d();const I=document.querySelector(".cart-product-delete-all-button");function L(t){let e=JSON.parse(localStorage.getItem("cart-products-list"))||[];const c=e.findIndex(a=>a._id===t);c!==-1&&(e.splice(c,1),localStorage.setItem("cart-products-list",JSON.stringify(e)),n(),d())}const m=document.querySelector(".cart-list");function u(t){const e=t.target.closest(".card-product-delete-button");if(e){const c=e.closest(".card_container_product").id;L(c),v()}}document.addEventListener("click",u);JSON.parse(localStorage.getItem("cart-products-list")).length&&m.addEventListener("click",u);function g(){localStorage.setItem("cart-products-list",JSON.stringify([])),l.innerHTML="",d(),m.removeEventListener("click",u),n()}JSON.parse(localStorage.getItem("cart-products-list")).length&&I.addEventListener("click",g);async function v(){const t=document.querySelector(".cart_total_cost"),c=(await JSON.parse(localStorage.getItem("cart-products-list"))||[]).reduce((a,r)=>a+r.price,0);t.textContent=`${c.toFixed(2)}`}v();const O=document.querySelector(".cart_form_button");JSON.parse(localStorage.getItem("cart-products-list")).length&&O.addEventListener("click",q);async function q(){const t=document.querySelector(".cart_form_input").value,c=JSON.parse(localStorage.getItem("cart-products-list")).map(s=>({productId:s._id,amount:s.amount}));if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)){p("Please, enter valid Email!");return}const r={email:t,products:c};try{const s=await f(r);g(),k(s)}catch(s){console.error("Error creating order:",s),p("Error creating order. Please try again later.")}}
 //# sourceMappingURL=commonHelpers.js.map
